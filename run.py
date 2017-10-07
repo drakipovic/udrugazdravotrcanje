f2eeb43c858e4badb3c82c408e90c729f0263f54
Dino Rakipovic <dino.rakipovic@gmail.com>
2017-10-07 20:40:14 +0200

    Admin can login

--- run.py
+++ run.py
@@ -5,11 +5,4 @@ from server.views import *
 
 
 if __name__ == '__main__':
-    db_name = app.config['SQLALCHEMY_DATABASE_URI'][10:]
-    if not os.path.exists(db_name):
-        os.system('touch {}'.format(db_name))
-        from server.models import User
-        from server.main import db
-        db.create_all()
-
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0')
--- server/views.py
+++ server/views.py
@@ -1,11 +1,29 @@
-from flask import render_template, request, redirect, jsonify, url_for, session
+from flask import render_template, request, redirect, jsonify, url_for, session, g
 
 from main import app
 from models import User
 
 
+@app.before_request
+def _before_request():
+    if session.get('logged_in'):
+        username = session['logged_in']
+        g.user = User.query.filter_by(username=username).first()
+        return
+    
+    if 'static' in request.url:
+        return
+
+    if 'admin' in request.url and request.method == 'GET':
+        return
+
+    if 'logged_in' not in session and 'admin' not in request.url:
+        return redirect(url_for('login_admin'))
+
+
 @app.route('/admin', methods=['GET', 'POST'])
 def login_admin():
+    print request
     if request.method == 'POST':
         username = request.json.get('username')
         password = request.json.get('password')
@@ -22,15 +40,25 @@ def login_admin():
         if remember_me:
             session.permanent = True
 
-        print session
-        print session['logged_in']
-
         return jsonify({'success': True})
 
     return render_template('login.html')
 
 
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    session.permanent = False
+
+    return redirect(url_for('login_admin'))
+
+
+@app.route('/')
 @app.route('/dashboard')
 def dashboard():
+    return render_template('dashboard.html', name=g.user.name, surname=g.user.surname)
 
-    return render_template('dashboard.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
