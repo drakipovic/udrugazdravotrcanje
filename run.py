aa735262c92a49fb8391d85bfc5d22f455f525d6
Dino Rakipovic <dino.rakipovic@gmail.com>
2018-01-09 21:38:37 +0100

    Changed lot of htmls

--- run.py
+++ run.py
@@ -4,8 +4,12 @@ from server.main import app, api
 from server.views import *
 from server.api import *
 
-api.add_resource(LeaguesView, '/leagues')
-api.add_resource(RaceView, '/races/<race_id>')
+
+api.add_resource(LeaguesEndpoint, '/leagues')
+api.add_resource(RaceEndpoint, '/races/<race_id>')
+api.add_resource(RaceResultsEndpoint, '/race-results/<race_id>')
+api.add_resource(RaceResultEndpoint, '/race-result/<race_result_id>')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
